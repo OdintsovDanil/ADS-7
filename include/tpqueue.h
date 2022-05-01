5a4ad7e10454d8a944760dcca8c9671ed0a8a291
OdintsovDanil <99181188+OdintsovDanil@users.noreply.github.com>
2022-05-01 21:23:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,62 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+    ITEM* start = nullptr;
+ public:
+    void push(const T& value);
+    T pop();
 };
 
+template<typename T>
+void TPQueue<T>::push(const T& value) {
+    ITEM* first = start;
+    if (first == nullptr) {
+        first = new ITEM;
+        start = first;
+        first->data = value;
+        first->next = nullptr;
+    } else {
+        ITEM* temp = new ITEM;
+        temp->data = value;
+        if (start->data.prior < temp->data.prior) {
+            temp->next = first;
+            start = temp;
+            return;
+        }
+        while (first != nullptr) {
+            if ((first->data.prior) >= (temp->data.prior) &&
+                ((first->next) == nullptr ||
+                    (((first->data.prior) < (temp->data.prior))
+                        || ((first->next) != nullptr &&
+                            first->next->data.prior < (temp->data.prior))))) {
+                temp->next = first->next;
+                first->next = temp;
+                return;
+            }
+            first = first->next;
+        }
+    }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+    ITEM* first = nullptr;
+    first = start;
+    if (first != nullptr) {
+        T out = first->data;
+        start = first->next;
+        return out;
+    }
+}
+
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
